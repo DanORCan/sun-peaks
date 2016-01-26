780df8f75e9a4ad7231bc58485f7648b37f05536
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2016-01-25 21:12:06 -0800

    Initial Commit

---
+++ sun-peaks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sun-peaks
+//
+//  Created by DANIEL OREILLY on 1/25/16.
+//  Copyright © 2016 DANIEL OREILLY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
