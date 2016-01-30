0231f33923c1d0d7c3b312e0e4397b3ee9f15521
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2016-01-29 22:54:34 -0800

    Second View Control Added with image and text

---
+++ sun-peaks/BlueButton.swift
@@ -0,0 +1,20 @@
+//
+//  BlueButton.swift
+//  sun-peaks
+//
+//  Created by DANIEL OREILLY on 1/27/16.
+//  Copyright © 2016 DANIEL OREILLY. All rights reserved.
+//
+
+import UIKit
+
+class BlueButton: UIButton {
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+        backgroundColor = UIColor(red: 133.0/255.0, green: 180.0/255.0, blue: 228.0/255.0, alpha: 0.6)
+        setTitleColor(UIColor.whiteColor(), forState: .Normal)
+    }
+    
+    
+}
